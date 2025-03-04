9294114a9b2ed37342facccee0cd3f87aedca424
Arwins-code <arwinsyahputra22@gmail.com>
2025-03-04 14:40:31 +0700

    update Main activity to show the fragment

--- app/src/main/java/com/arwin/bigmusic/MainActivity.kt
+++ app/src/main/java/com/arwin/bigmusic/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.arwin.bigmusic
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentTransaction
+import com.arwin.bigmusic.databinding.ActivityMainBinding
+import com.arwin.bigmusic.ui.fragment.MusicListFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, MusicListFragment())
+        transaction.commit()
     }
 }
